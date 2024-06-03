acbc20a74678d320e51ca5b18c971c6abdbd08a4
Danilo Almeida Alves <71449479+b0d3-crypt@users.noreply.github.com>
2024-06-03 20:32:42 -0300

    adição do swagger

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, HttpStatus, Post, Res } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Res } from "@nestjs/common";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 import { Response } from "express";
 import { UsuarioDTO } from "src/dtos/usuario.dto";
 import { Usuario } from "src/entities/usuario.entity";
@@ -12,12 +13,19 @@ export class UsuarioController {
     ){}
 
     @Get()
+    @HttpCode(200)
+    @ApiOperation({ summary: 'Listar todos os usuários' })
+    @ApiResponse({ status: 200, description: 'Lista de usuários retornada com sucesso', type: Usuario, isArray: true })
     async findAll(): Promise<Usuario[]> {
         const usuarios: Usuario[] = await this._usuarioService.findAll();
         return usuarios;
     } 
 
     @Post()
+    @HttpCode(201)
+    @ApiOperation({ summary: 'Criar um novo usuário' }) 
+    @ApiResponse({ status: 201, description: 'Usuário criado com sucesso' }) 
+    @ApiResponse({ status: 500, description: 'Erro interno do servidor' })
     async save(@Body() usuarioDTO: UsuarioDTO, @Res() res: Response) {
         try {
             await this._usuarioService.create(usuarioDTO);
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import * as bcrypt from 'bcrypt';
 import { PessoaBuilder } from "src/builders/pessoa.builder";
 import { UsuarioBuilder } from "src/builders/usuario.builder";
 import { UsuarioDTO } from "src/dtos/usuario.dto";
@@ -7,6 +8,7 @@ import { Pessoa } from "src/entities/pessoa.entity";
 import { Usuario } from "src/entities/usuario.entity";
 import { TransactionManager } from "src/tools/transaction.manager";
 import { Repository } from "typeorm";
+
 
 @Injectable()
 export class UsuarioService {
@@ -24,7 +26,7 @@ export class UsuarioService {
         return this._transactionManager.transaction(async (entityManager) => {
             let pessoa = this._setPessoa(usuarioDTO);
             pessoa = await entityManager.save(pessoa);
-            const usuario = this._setUsuario(usuarioDTO, pessoa);
+            const usuario = await this._setUsuario(usuarioDTO, pessoa);
             await entityManager.save(usuario);
         })
     }
@@ -36,10 +38,10 @@ export class UsuarioService {
             .build();
     }
 
-    private _setUsuario(usuarioDTO: UsuarioDTO, pessoa: Pessoa) {
+    private async _setUsuario(usuarioDTO: UsuarioDTO, pessoa: Pessoa) {
         return new UsuarioBuilder()
             .setNick(usuarioDTO.nick)
-            .setPassword(usuarioDTO.password)
+            .setPassword(await bcrypt.hash(usuarioDTO.password, 10))
             .setPessoa(pessoa)
             .build();
     }
